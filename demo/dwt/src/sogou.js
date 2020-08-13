b2f99ad328d1b73238ba20b2c87c1e370fc683e4
linjianghe <linjianghe@tencent.com>
2020-08-13 11:55:29 +0800

    feat: 新增 dwt demo

--- demo/dwt/src/sogou.js
+++ demo/dwt/src/sogou.js
@@ -1,18 +1,23 @@
 const _ = require('lodash');
 
-fetch('/cgi-bin/a/b/demo_cgi')
-  .then((result) => {
-    return result.json();
-  })
-  .then((jsonData) => {
-    console.log(jsonData);
+function bindClickEvent() {
+  $('#btn').on('click', function () {
+    fetch('/cgi-bin/a/b/demo_cgi')
+      .then((result) => {
+        return result.json();
+      })
+      .then((jsonData) => {
+        console.log(jsonData);
 
-    // 修改消息
-    document.querySelector('#msg').innerText = _.concat('来自接口返回：',jsonData.result.description).join(' ');
-
-    // 追加 DOM
-    const newDiv = document.createElement('div');
-    newDiv.innerText = 'loaded!';
-    newDiv.setAttribute('id', 'loaded');
-    document.body.appendChild(newDiv);
+        // 修改消息
+        $('#msg').text(_.concat('来自接口返回：', jsonData.result.description).join(' '));
+        $('#msg').removeClass('alert-info').addClass('alert-success').addClass('msg-loaded');
+      });
   });
+}
+
+$(function () {
+  console.log('jQuery loaded!');
+
+  bindClickEvent();
+});
