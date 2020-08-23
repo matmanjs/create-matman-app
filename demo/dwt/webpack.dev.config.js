50a2950e9bcf34d78060cb81931b09720c243cb5
helinjiang <onehlj@qq.com>
2020-08-23 18:35:33 +0800

    feat: 优化 demo，覆盖率

--- demo/dwt/webpack.dev.config.js
+++ demo/dwt/webpack.dev.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+const webpackConfig = {
   mode: 'development',
   entry: {
     sogou: './src/sogou.js',
@@ -10,6 +10,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].dev.bundle.js',
+  },
+  module: {
+    rules: [],
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -19,3 +22,19 @@ module.exports = {
     }),
   ],
 };
+
+// 端对端测试时，需要特殊处理以便能够获知代码覆盖率
+if (process.env.ENABLE_E2E_TEST === '1') {
+  webpackConfig.devtool = 'sourcemap';
+  webpackConfig.module.rules.push({
+    test: /\.js$/,
+    use: {
+      loader: 'istanbul-instrumenter-loader',
+      options: { esModules: true },
+    },
+    enforce: 'post',
+    exclude: /node_modules|.\spec\.js$/,
+  });
+}
+
+module.exports = webpackConfig;
