8542090e33e76b89237695e36d300658a438c755
helinjiang <onehlj@qq.com>
2020-10-09 14:21:41 +0800

    feat: 为 mocha-ts 项目增加单元测试和单测覆盖率统计

---
+++ scripts/template/project-ts/src/util.ts
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+export function getDescribeForRes(msg: any): string {
+  return _.concat('来自接口返回：', (typeof msg === 'object') ? JSON.stringify(msg) : msg).join(' ');
+}
+
+export function iAmNotCalled() {
+  // 这段代码没有被调用，因此不会被覆盖率统计到！
+  console.log('I am not called in util.js!');
+}
